8620f18449883a069765f0380f449df0120f6bbc
temanbrcom <andrei.temnikov@broadcom.com>
2019-10-09 15:51:34 +0000

    Code actions

--- lsp4j-server/src/main/java/lsp4j/server/MyLanguageServer.java
+++ lsp4j-server/src/main/java/lsp4j/server/MyLanguageServer.java
@@ -1,12 +1,23 @@
 package lsp4j.server;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
+import org.eclipse.lsp4j.ApplyWorkspaceEditParams;
 import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.CodeActionKind;
+import org.eclipse.lsp4j.CodeActionOptions;
 import org.eclipse.lsp4j.CodeActionParams;
 import org.eclipse.lsp4j.Command;
 import org.eclipse.lsp4j.CompletionItem;
@@ -22,6 +33,7 @@ import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.ExecuteCommandOptions;
 import org.eclipse.lsp4j.ExecuteCommandParams;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
@@ -30,6 +42,8 @@ import org.eclipse.lsp4j.PublishDiagnosticsParams;
 import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.ServerCapabilities;
 import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4j.WorkspaceEdit;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageClientAware;
@@ -40,6 +54,7 @@ import org.eclipse.lsp4j.services.WorkspaceService;
 public class MyLanguageServer implements LanguageServer, TextDocumentService, WorkspaceService, LanguageClientAware {
 
     private LanguageClient client;
+    private Map<String, String> model = new HashMap<>();
 
     @Override
     public void connect(LanguageClient client) {
@@ -54,6 +69,8 @@ public class MyLanguageServer implements LanguageServer, TextDocumentService, Wo
         completionOptions.setResolveProvider(true);
         capabilities.setCompletionProvider(completionOptions);
         // TODO allow code actions and execute command requests
+        capabilities.setCodeActionProvider(new CodeActionOptions(Arrays.asList(CodeActionKind.QuickFix)));
+        capabilities.setExecuteCommandProvider(new ExecuteCommandOptions(Collections.singletonList("myToLowerCase")));
         return CompletableFuture.completedFuture(new InitializeResult(capabilities));
     }
 
@@ -99,7 +116,7 @@ public class MyLanguageServer implements LanguageServer, TextDocumentService, Wo
 
     @Override
     public void didClose(DidCloseTextDocumentParams params) {
-
+        model.remove(params.getTextDocument().getUri());
     }
 
     @Override
@@ -110,6 +127,7 @@ public class MyLanguageServer implements LanguageServer, TextDocumentService, Wo
     static Pattern PATTERN = Pattern.compile("\\b[A-Z]{2,}\\b");
 
     protected void validateTextDocument(String uri, String content) {
+        model.put(uri, content);
         PublishDiagnosticsParams diagnostics = new PublishDiagnosticsParams();
         diagnostics.setUri(uri);
         String[] lines = content.split("\\n");
@@ -151,14 +169,33 @@ public class MyLanguageServer implements LanguageServer, TextDocumentService, Wo
 
     @Override
     public CompletableFuture<List<Either<Command, CodeAction>>> codeAction(CodeActionParams params) {
+        return CompletableFuture.completedFuture(
+                params.getContext().getDiagnostics().stream().filter(it -> it.getSource().equals("ex")).map(it -> {
+                    CodeAction action = new CodeAction("To Lower Case");
+                    action.setDiagnostics(Collections.singletonList(it));
+                    action.setKind(CodeActionKind.QuickFix);
+                    action.setCommand(new Command("To Lower Case", "myToLowerCase",
+                            Arrays.asList(params.getTextDocument().getUri(), params.getRange())));
+                    // action.setEdit(new
+                    // WorkspaceEdit(Collections.singletonMap(params.getTextDocument().getUri(),
+                    // Collections.singletonList(new TextEdit(it.getRange(), "foo")))));
+                    return action;
+                }).map(it -> Either.<Command, CodeAction>forRight(it)).collect(Collectors.toList()));
+
         // TODO provide to lowercase quick fixes for in uppercase warnings
-        return TextDocumentService.super.codeAction(params);
+        // return TextDocumentService.super.codeAction(params);
     }
 
     @Override
     public CompletableFuture<Object> executeCommand(ExecuteCommandParams params) {
         // TODO apply the quick fix
-        return WorkspaceService.super.executeCommand(params);
+        if ("myToLowerCase".equals(params.getCommand())) {
+            client.applyEdit(new ApplyWorkspaceEditParams(
+                    new WorkspaceEdit(Collections.singletonMap(new Gson().fromJson((JsonElement)params.getArguments().get(0), String.class),
+                            Collections.singletonList(new TextEdit(new Gson().fromJson((JsonElement)params.getArguments().get(1), Range.class), "foo"))))));
+        }
+
+        return CompletableFuture.completedFuture(new Object());
     }
 
 }
